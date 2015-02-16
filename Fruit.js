c6c46b8f930156b7803672c2eb96c0c92b8c8dde
Connor Ring <crv9557@saintjoe.edu>
2015-02-16 12:14:39 -0500

    In class exercise for Postma  Please enter the commit message for your changes. Lines starting

---
+++ Fruit.js
@@ -0,0 +1,6 @@
+var fruits ={
+       	"Apple": ['red','round', 'healthy'],
+	"Orange": ['orange', 'spherical', 'vitamin C'],
+	"Grape": ['purple', 'round', 'vitamin B'],
+	"Pear": [ 'green', 'roundish', 'vitamin D']
+
